20438913ef2340e446bb0a8bb29b0a344685861e
monarianti <monarianti21@gmail.com>
2020-04-26 15:16:07 +0800

    Update InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -4,20 +4,20 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
-  int jumlahSks;
+  int jumlahSKS;
   double IPK;
 
-  System.out.print("Masukkan nama anda : ");
+  System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
   System.out.println("Hai, "+nama);
 
-  System.out.print("Masukkan jumlah SKS yang sudah ditempuh : ");
-  jumlahSks = scanner.nextInt();
+  System.out.print("Masukkan jumlah SKS yang sudah ditempuh: ");
+  jumlahSKS = scanner.nextInt();
 
-  System.out.print("Masukkan IPK Sementara : ");
+  System.out.print("Masukkan IPK sementara: ");
   IPK = scanner.nextDouble();
 
-  double jumlah = IPK * jumlahSks;
+  Double jumlah = IPK * jumlahSKS;
   System.out.print("Total Nilai = "+jumlah);
  }
 }
