89d7b42b80c7b0e2ae7a4a5fe82aad177816d4e3
monarianti <monarianti21@gmail.com>
2020-04-26 12:35:51 +0800

    memanggil method dari banyak Class di package

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("==Data Mata Kuliah=================");
+  System.out.println("Kode : 18630598");
+  System.out.println("Nama : Monarianti");
+  System.out.println("===================================");
+ }
+}
