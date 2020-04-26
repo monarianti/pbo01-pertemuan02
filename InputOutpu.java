feacbef47e1152616de428b07ebb14f442dee6b4
monarianti <monarianti21@gmail.com>
2020-04-26 12:55:58 +0800

    Buat Output

--- InputOutpu.java
+++ InputOutpu.java
@@ -1,8 +1,10 @@
 public class InputOutput{
  public static void main (String [] args){
   Scanner scanner = new Scanner(System.in);
-String nama;
-System.out.print("Masukkan nama anda : ");
-nama = scanner.nextLine();
+  String nama;
+  System.out.print("Masukkan nama anda : ");
+  nama = scanner.nextLine();
+  System.out.println("Hai, "+nama);
+
  }
 }
